817b168b3eb94774d25b1565cac5255c7f1fe8e1
Konstantin Stoyakin <gavrilbatya@yandex.ru>
2025-06-01 21:34:40 +0300

    Создать каркас приложения ShareIt

---
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,52 @@
+package ru.practicum.shareit.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleConflictException(final ConflictException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        String errors = e.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return new ErrorResponse("Ошибка валидации: " + errors);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingRequestHeaderException(final MissingRequestHeaderException e) {
+        return new ErrorResponse("Отсутствует обязательный заголовок: " + e.getHeaderName());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleNullPointerException(final NullPointerException e) {
+        return new ErrorResponse("Обнаружена нулевая ссылка: " + e.getMessage());
+    }
+}
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class Item {
+    private Long id;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Описание не может быть пустым")
+    private String description;
+
+    @NotNull(message = "Статус доступности не может быть пустым")
+    private Boolean available;
+
+    private Long ownerId;
 }
