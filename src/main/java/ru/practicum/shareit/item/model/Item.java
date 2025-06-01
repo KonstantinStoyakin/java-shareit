8d417d87df8aea710658ef2335498f9c976647a5
Konstantin Stoyakin <gavrilbatya@yandex.ru>
2025-06-01 21:39:56 +0300

    Создать каркас приложения ShareIt

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,10 +2,16 @@ package ru.practicum.shareit.item.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
     private Long id;
 
